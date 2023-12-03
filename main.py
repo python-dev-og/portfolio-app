b9e857d27e29d2181c34af4e0721478a705abbee
Ogochukwu Ozotta <88560609+ogochukwu-ozotta@users.noreply.github.com>
2023-12-03 03:50:21 -0600

    Create columns, images and description

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Ogochukwu Ozotta")
+    content = """
+    Hi, I am Ogochukwu "OG" Ozotta! I am a Python programmer, 
+    a dynamic and forward-thinking DevOps, Site Reliability Engineering (SRE), and Cloud Computing expert. 
+    With a passion for innovation and a relentless pursuit of excellence, 
+    I have carved a niche in the tech industry, specializing in creating robust, 
+    scalable, and efficient digital solutions.
+    """
+    st.info(content)

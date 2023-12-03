199d04afee29df10845ba2702906d6c0b4d5cf62
Ogochukwu Ozotta <88560609+ogochukwu-ozotta@users.noreply.github.com>
2023-12-03 14:20:26 -0600

    Added the env and finished the app

--- Home.py
+++ Home.py
@@ -26,7 +26,7 @@ st.write(content2)
 
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
-df = pd.read_csv("data.csv", sep=";")
+df = pd.read_csv("data.csv")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(message):
@@ -7,7 +8,7 @@ def send_email(message):
     port = 465
 
     username = "ogochukwu.ozotta@gmail.com"
-    password = "ypzavcxqsichjqri"
+    password = os.getenv("PASSWORD")
 
     receiver = "ogochukwu.ozotta@gmail.com"
     context = ssl.create_default_context()

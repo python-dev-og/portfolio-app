4ee5ea18c08fb6fdfcf97b5931750c91bf387edb
Ogochukwu Ozotta <88560609+ogochukwu-ozotta@users.noreply.github.com>
2023-12-03 06:42:41 -0600

    Send email

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
+
+st.set_page_config(layout="wide")
+
 st.header("Contact Me")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+    if button:
+        print("Pressed")
--- pages/Resume.py
+++ pages/Resume.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 st.header("Resume")
+
